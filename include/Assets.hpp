91cee536f1e2c483442437225669646f49d57036
liljekvist <mikael0225@outlook.com>
2023-01-06 00:11:27 +0100

    Fixade linux build för assets

--- include/Assets.hpp
+++ include/Assets.hpp
@@ -19,9 +19,9 @@ class Assets
                                                    // of root we need to jump one more step out
         missile.loadFromFile("../assets/Missile.png");
         background.loadFromFile("../assets/background.jpg");
-        explotionSheet.loadFromFile("../assets/explotionsheet.png");
-        metioriteSheet.loadFromFile("../assets/metioritesheet.png");
-        textFont.loadFromFile("../assets/NFagave.ttf");
+        explotion_sheet.loadFromFile("../assets/explotionsheet.png");
+        metiorite_sheet.loadFromFile("../assets/metioritesheet.png");
+        text_font.loadFromFile("../assets/NFagave.ttf");
 #elif _WIN32
         tower.loadFromFile("assets/Tower.png");
         missile.loadFromFile("assets/Missile.png");
